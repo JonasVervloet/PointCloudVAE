c434b47bf2d131258ff9be5c95a8d493aff1db57
JonasVervloet <vervloetjonas@hotmail.com>
2020-08-05 00:11:49 +0200

    add VAE

--- standard_network/network/vae_network.py
+++ standard_network/network/vae_network.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 from standard_network.encoder.middle_encoder import MiddleEncoder
@@ -9,9 +10,9 @@ from standard_network.decoder.outside_decoder import OutsideDecoder
 from standard_network.decoder.inside_decoder import InsideDecoder
 
 
-class PointCloudAE(nn.Module):
-    def __init__(self, encoders, decoders):
-        super(PointCloudAE, self).__init__()
+class PointCloudVAE(nn.Module):
+    def __init__(self, encoders, decoders, lat_size):
+        super(PointCloudVAE, self).__init__()
 
         assert(len(encoders) == 3)
         assert(isinstance(encoders[0], OutsideEncoder))
@@ -26,6 +27,9 @@ class PointCloudAE(nn.Module):
         self.outside_encoder = encoders[0]
         self.middle_encoder = encoders[1]
         self.inside_encoder = encoders[2]
+
+        self.enc_mu = nn.Linear(lat_size, lat_size)
+        self.enc_var = nn.Linear(lat_size, lat_size)
 
         self.inside_decoder = decoders[0]
         self.middle_decoder = decoders[1]
@@ -42,9 +46,15 @@ class PointCloudAE(nn.Module):
             middle_enc_points, middle_enc_features, middle_enc_batch
         )
 
-        in_dec_points, in_dec_features, in_dec_batch = self.inside_decoder(
-            in_enc_features
-        )
+        mean = self.enc_mu(in_enc_features)
+        variance = self.enc_var(in_enc_features)
+        std = torch.exp(variance*0.5)
+        eps = torch.randn_like(std)
+
+        x_sample = eps.mul(std).add(mean)
+
+        in_dec_points, in_dec_features, in_dec_batch = self.inside_decoder(x_sample)
+
         middle_dec_points, middle_dec_features, middle_dec_batch = self.middle_decoder(
             in_dec_points, in_dec_features, in_dec_batch
         )
@@ -58,5 +68,4 @@ class PointCloudAE(nn.Module):
         output_points_list = [out_dec_points, middle_dec_points, in_dec_points]
         output_batch_list = [out_dec_batch, middle_dec_batch, in_dec_batch]
 
-        return input_points_list, input_batch_list, output_points_list, output_batch_list, in_enc_features
-
+        return input_points_list, input_batch_list, output_points_list, output_batch_list, mean, variance
